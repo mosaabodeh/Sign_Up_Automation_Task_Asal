bed93349a55685941b3e4d2bac246921fde67036
moodeh <mosaab.cp@gmail.com>
2026-07-12 11:04:46 +0300

    Fix the Spinner Select Element By Given Name

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -4,7 +4,6 @@ import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.locators.ElementKey;
